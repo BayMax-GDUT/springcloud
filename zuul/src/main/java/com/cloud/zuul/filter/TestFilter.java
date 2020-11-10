8b1d8848d2c6ef2013895b8e5b0f07593de3d027
BayMax-GDUT <845625823@qq.com>
2020-11-10 17:23:36 +0800

    Zuul过滤器

---
+++ zuul/src/main/java/com/cloud/zuul/filter/TestFilter.java
@@ -0,0 +1,30 @@
+package com.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("过滤器1执行");
+        return null;
+    }
+}
